c56b0ca805b8321fb8e812252784853ada0a7d89
Viktoria Czaran <vczaran@gmail.com>
2023-05-23 15:07:11 -0400

    Working on chessboard

---
+++ Chess/board.rb
@@ -0,0 +1,39 @@
+require_relative "piece"
+
+
+class Board
+
+    def initialize
+        @rows = Array.new(8) {Array.new(8, [])}
+        @rows.each do |row|
+            2.times do 
+                row << Piece.new(:white)
+            end
+            #  @rows.index(row)
+            # row.each do |ele|
+            #     p ele.pos
+            # end
+        end
+
+    end
+
+    p Board.new
+
+    def [](pos)
+      row, col = pos
+      @rows[row][col]
+    end
+
+
+    def []=(pos, val)
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+
+    def move_piece(start_pos, end_pos)
+
+    end
+
+
+end
---
+++ Chess/piece.rb
@@ -0,0 +1,13 @@
+class Piece
+
+    def initialize(color)
+        @color = :color
+        # @board = Board.new
+        # @pos = [pos]
+    end
+
+
+
+
+
+end
