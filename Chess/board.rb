aec6b38b8569b5c6aa1c7ba05943444807c97d05
Jordy <jordycorporan95@gmail.com>
2023-05-23 15:41:14 -0400

    Starting board

--- Chess/board.rb
+++ Chess/board.rb
@@ -5,19 +5,45 @@ class Board
 
     def initialize
         @rows = Array.new(8) {Array.new(8)}
-        # @rows.each do |row|
-        #     2.times do 
-        #         row << Piece.new(:white)
-        #     end
-            #  @rows.index(row)
-            # row.each do |ele|
-            #     p ele.pos
+        @rows.each do |pos|
+            if pos == 0 || pos == 1
+                rows
+
+
+            # row.each_with_index do |ele,idx2|
+            #     pos = [idx1, idx2]
+            #     if pos.first == 0 || pos.first == 1
+            #         ele = Piece.new(:white)
+            #     end
             # end
-        # end
+
+
+
+        end
 
     end
+    #     if row == 0 || row == 1
+    #         @rows[row] = Piece.new(:white)
+    #     end
+    # end
 
-    p Board.new
+    #     # (0..7).each do |pos|
+            # if pos == 0 || pos == 7
+            #     row[pos] = "Test"
+
+
+        # 2.times do
+        #     row << Piece.new(:white)
+        # end
+        #  @rows.index(row)
+        # row.each do |ele|
+        #     p ele.pos
+        # end
+
+
+
+
+    # p Board.new
 
     def [](pos)
       row, col = pos
