d293b7c673dd3fc676b0f2c10319f7717eabbbb6
Jordy <jordycorporan95@gmail.com>
2023-05-23 12:17:36 -0400

    Added project file

---
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager_class.rb
@@ -0,0 +1,10 @@
+require_relative "employee_class"
+
+class Manager < Employee
+  # def initialize(name,title,salary,boss,employees)
+  #   super(name,title,salary,boss)
+  #   @employees = []
+  # end
+
+
+end
