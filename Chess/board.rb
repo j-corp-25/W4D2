da3378d42e63b4fa6ddd9f7ced340596acc58cbc
Viktoria Czaran <vczaran@gmail.com>
2023-05-23 16:48:25 -0400

    Completed phase I

--- Chess/board.rb
+++ Chess/board.rb
@@ -14,65 +14,9 @@ class Board
                 end
             end
         end
-        
-            # (0..7).each do |pos|
-        #     @rows[pos] = "Test"
-        # end
-
-            # if pos == 0 || pos == 1
-        #         7.times do
-        #             @rows[pos] = "Test"
-        #         end
-        #     end
-        # end
-
-            # (0..7).each do |pos|
-            #     if pos == 0 || pos == 7
-            #         row[pos] = "Test"
-            #     end
-            # end
-
-
-
-
-            # if pos == 0 || pos == 1
-            #     rows
-
-
-            # row.each_with_index do |ele,idx2|
-            #     pos = [idx1, idx2]
-            #     if pos.first == 0 || pos.first == 1
-            #         ele = Piece.new(:white)
-            #     end
-            # end
-
-
-
-        # end
 
     end
-    #     if row == 0 || row == 1
-    #         @rows[row] = Piece.new(:white)
-    #     end
-    # end
 
-    #     # (0..7).each do |pos|
-            # if pos == 0 || pos == 7
-            #     row[pos] = "Test"
-
-
-        # 2.times do
-        #     row << Piece.new(:white)
-        # end
-        #  @rows.index(row)
-        # row.each do |ele|
-        #     p ele.pos
-        # end
-
-
-
-
-    # p Board.new
 
     def [](pos)
       row, col = pos
@@ -87,9 +31,9 @@ class Board
 
 
     def move_piece(start_pos, end_pos)
-        raise "no piece there" if @row.start_pos == nil
+        raise "no piece there" if self[start_pos] == nil
 
-
+       self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
     end
 
 
